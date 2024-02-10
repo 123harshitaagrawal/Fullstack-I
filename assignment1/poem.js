382fa2a400fa773acbfa002389ec9043b33bd424
123harshitaagrawal <agrawalhr_1@rknec.edu>
2024-02-10 14:38:49 +0530

    requirement 2

---
+++ assignment1/poem.js
@@ -0,0 +1,14 @@
+
+
+const fs = require('fs');
+
+
+fs.readFile('poem.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+    
+    console.log('Contents of poem.txt:');
+    console.log(data);
+});
