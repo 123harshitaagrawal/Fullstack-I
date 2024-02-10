e552067f4e4a68f6e0f4fa4d306adef0877e31c7
123harshitaagrawal <agrawalhr_1@rknec.edu>
2024-02-10 14:10:43 +0530

    requirement 1

---
+++ assignment1/test2.js
@@ -0,0 +1,7 @@
+// test2.js
+
+const dateUtil = require('./dateUtil');
+
+console.log("Testing module import as a variable:");
+console.log("Leap year test (2024):", dateUtil.isLeapYear(2024)); // Should log true
+console.log("Non-leap year test (2023):", dateUtil.isLeapYear(2023)); // Should log false
